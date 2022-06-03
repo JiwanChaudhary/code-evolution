ac163e1fd7798e1f538f058bda164d3ca66139f1
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-06-03 22:57:49 +0545

    ReactJS from codeEvolution

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+// import Hello from './components/Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Bruce" heroName="Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Spiderman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman" />
+
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Spiderman" />
+      <Welcome name="Diana" heroName="Wonder Woman" />
+      {/* <Hello /> */}
+    </div>
+  );
+}
+
+export default App;
